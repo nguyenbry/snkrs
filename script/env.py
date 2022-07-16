dc8c44e7ee58be33bacd18177377389a3c8a3854
Bryan Nguyen <brng@seas.upenn.edu>
2022-07-16 13:07:54 -0400

    yeah

--- script/env.py
+++
@@ -1,2 +0,0 @@
-TENANT_ID = "lacedboston-0p9a9"
-GOOGLE_API_KEY = "AIzaSyBH1NS5RuSMLekpp-SUrQCVa-cDbErzpo4"
--- script/src/utils/utils.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def pprint(data):
-  print(json.dumps(data, indent=2))
-  
-def save_json(j, filename):
-  with open(filename, 'w+') as file:
-    file.write(json.dumps(j, indent=2))
